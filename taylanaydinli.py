52b8f2e5ee587d2a148ff0ee210003e821017e90
Taylan Aydinli <taylanaydinli@gmail.com>
2017-04-12 19:00:16 +0300

    Add Let's Encrypt challenge response

--- taylanaydinli.py
+++ taylanaydinli.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/.well-known/acme-challenge/<id>")
+def acme_challenge(id):
+    return "vBlS7Sc9Nc5nb2lfVrVKhbhjEZd_OF7x__3aXQgrlHA.CyBrusGjiby7J5p0SvG0zGCwwXucb0wcMyT4mLHFfzc"
+
 if __name__ == "__main__":
     app.run()
